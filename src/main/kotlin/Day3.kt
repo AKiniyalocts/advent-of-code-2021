cf7a609c3678a6a8ef6a17e24b23aa8a235a603a
Anthony Kiniyalocts <kiniyaloca1@gmail.com>
2021-12-14 12:10:31 -0500

    finish day 3

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -1,7 +1,8 @@
 import java.io.File
+import java.lang.StringBuilder
 
 fun main() {
-    PartOne()
+    PartTwo()
 }
 
 
@@ -10,16 +11,135 @@ private fun PartOne(){
         .readText()
         .split("\n")
 
-    val mapped = mutableListOf<MutableList<String>>()
+    val positionCounts: MutableList<PositionCount> = data.first().mapIndexed { index, value ->
+        PositionCount(
+            position = index,
+            numOfOnes = 0,
+            numOfZeros = 0
+        )
+    }.toMutableList()
 
 
-    data.forEachIndexed { index, value ->
-        val indexList = mutableListOf<String>()
-
-        value.forEachIndexed { index, char ->
-            val row = mapped.getOrElse(index){ mutableListOf()}
-            row.add(char.toString())
+    data.forEach {
+        it.forEachIndexed { index, value ->
+            val positionCount = positionCounts[index]
+            if(value.toString() == "1"){
+                positionCount.numOfOnes++
+            }else{
+                positionCount.numOfZeros++
+            }
         }
     }
-    print(data)
+    println(positionCounts)
+
+    val gammaRateBuilder = StringBuilder()
+    positionCounts.forEach {
+
+        if(it.numOfOnes > it.numOfZeros){
+            gammaRateBuilder.append("1")
+        }else if(it.numOfZeros > it.numOfOnes){
+            gammaRateBuilder.append("0")
+        }else {
+            println("there was a tie!!")
+        }
+    }
+    println("gamma rate: $gammaRateBuilder")
+
+    val epsilonRateBuilder = StringBuilder()
+    positionCounts.forEach {
+        if(it.numOfOnes < it.numOfZeros){
+            epsilonRateBuilder.append("1")
+        }else{
+            epsilonRateBuilder.append("0")
+        }
+    }
+
+    println("epsilon rate: $epsilonRateBuilder")
+
+    println("${gammaRateBuilder.toString().toInt(2).times(epsilonRateBuilder.toString().toInt(2))} ")
+}
+
+data class PositionCount(val position: Int, var numOfOnes: Int, var numOfZeros: Int){
+    val mostCommon: String
+    get() {
+        return if(numOfOnes == numOfZeros) {
+             "1"
+        }else{
+            if(numOfOnes > numOfZeros) "1" else "0"
+        }
+    }
+
+    val leastCommon: String
+    get() {
+        return if(numOfOnes == numOfZeros){
+            return "0"
+        }else{
+            if(numOfOnes < numOfZeros) "1" else "0"
+        }
+    }
+}
+
+private fun PartTwo(){
+    val data = File("data/day_3.txt")
+        .readText()
+        .split("\n")
+
+    var positionCounts: MutableList<PositionCount> = initialPositionCounts(data)
+
+    var currentIndex = 0
+
+    var keptC02ratings: List<String> = data
+
+    var keptOxygenRatings: List<String> = data
+
+    while(keptOxygenRatings.size > 1){
+        positionCounts = initialPositionCounts(keptOxygenRatings)
+        keptOxygenRatings = filterByPredicate(keptOxygenRatings, currentIndex, positionCounts[currentIndex].mostCommon)
+
+        currentIndex++
+    }
+    println("oxygen generator rating: ${keptOxygenRatings.first()}")
+
+
+    currentIndex = 0
+    while(keptC02ratings.size > 1){
+        positionCounts = initialPositionCounts(keptC02ratings)
+        keptC02ratings = filterByPredicate(keptC02ratings, currentIndex, positionCounts[currentIndex].leastCommon)
+        currentIndex++
+    }
+
+
+    println("C02 scrubber rating: ${keptC02ratings.first()}")
+
+    println("Life support rating: ${keptOxygenRatings.first().toInt(2) * keptC02ratings.first().toInt(2)}")
+}
+
+private fun initialPositionCounts(data: List<String>): MutableList<PositionCount> {
+    var positionCounts: MutableList<PositionCount> = data.first().mapIndexed { index, _ ->
+        PositionCount(
+            position = index,
+            numOfOnes = 0,
+            numOfZeros = 0
+        )
+    }.toMutableList()
+
+    data.forEachIndexed { index, fullNumber ->
+
+        fullNumber.forEachIndexed { individualIndex, value ->
+            val positionCount = positionCounts[individualIndex]
+            if (value.toString() == "1") {
+                positionCount.numOfOnes++
+            } else {
+                positionCount.numOfZeros++
+            }
+        }
+    }
+    return positionCounts
+}
+
+private fun filterByPredicate(data: List<String>, currentIndex: Int, predicate: String): List<String>{
+    return data.filter {
+        val charAtIndex = it[currentIndex].toString()
+        charAtIndex == predicate
+    }
 }
